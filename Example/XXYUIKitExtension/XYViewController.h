85a77332cb30ca971f952ec7ae35c767009db619
jason <jasondreamxu@163.com>
2020-12-27 01:40:53 +0800

    Initial commit

---
+++ Example/XXYUIKitExtension/XYViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYViewController.h
+//  XXYUIKitExtension
+//
+//  Created by JasonDraem on 12/27/2020.
+//  Copyright (c) 2020 JasonDraem. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XYViewController : UIViewController
+
+@end
---
+++ Example/XXYUIKitExtension/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XXYUIKitExtension
+//
+//  Created by JasonDraem on 12/27/2020.
+//  Copyright (c) 2020 JasonDraem. All rights reserved.
+//
+
+@import UIKit;
+#import "XYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYAppDelegate class]));
+    }
+}
