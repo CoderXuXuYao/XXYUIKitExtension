de3a859817123b83a931220b63a0195afb7405d0
jason <jasondreamxu@163.com>
2020-12-27 04:07:07 +0800

    添加测试代码

---
+++ Example/XXYUIKitExtension/XYRootViewController.h
@@ -0,0 +1,17 @@
+//
+//  XYRootViewController.h
+//  XXYUIKitExtension_Example
+//
+//  Created by 许须耀 on 2020/12/27.
+//  Copyright © 2020 JasonDraem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XYRootViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/XXYUIKitExtension/XYRootViewController.m
@@ -0,0 +1,32 @@
+//
+//  XYRootViewController.m
+//  XXYUIKitExtension_Example
+//
+//  Created by 许须耀 on 2020/12/27.
+//  Copyright © 2020 JasonDraem. All rights reserved.
+//
+
+#import "XYRootViewController.h"
+
+@interface XYRootViewController ()
+
+@end
+
+@implementation XYRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
